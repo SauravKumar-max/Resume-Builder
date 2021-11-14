a617a9b10fb15c3a217d509d559c0b094c9987bc
Saurav Kumar <maxsaurav237@gmail.com>
2021-11-14 10:39:52 +0530

    fixed useEffect import route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
-import { useRef } from "react";
-import { useEffect } from "react/cjs/react.development";
+import { useRef, useEffect } from "react";
 import { useDetails } from "../context/DetailsProvider";
 
 export function Navbar(){
     const { detailState:{ step } } = useDetails();
-
     const scrollToTopRef = useRef(null);
+
     useEffect(() => {
         scrollToTopRef.current.scrollIntoView();
     }, [step])
