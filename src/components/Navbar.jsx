c136c0c96bdfd132c5bc1f8fdf80809562640061
Saurav Kumar <maxsaurav237@gmail.com>
2021-11-14 07:02:34 +0530

    test for deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
-import { useRef } from "react";
-import { useEffect } from "react/cjs/react.development";
-import { useDetails } from "../context/DetailsProvider";
+// import { useRef } from "react";
+// import { useEffect } from "react/cjs/react.development";
+// import { useDetails } from "../context/DetailsProvider";
 
 export function Navbar(){
-    const { detailState:{ step } } = useDetails();
+    // const { detailState:{ step } } = useDetails();
 
-    const scrollToTopRef = useRef(null);
-    useEffect(() => {
-        scrollToTopRef.current.scrollIntoView();
-    }, [step])
+    // const scrollToTopRef = useRef(null);
+    // useEffect(() => {
+    //     scrollToTopRef.current.scrollIntoView();
+    // }, [step])
 
     return(
-        <nav ref={scrollToTopRef} className="text-white bg-primary p-4 sticky flex items-center justify-between"> 
+        <nav className="text-white bg-primary p-4 sticky flex items-center justify-between"> 
             <Link to="/" className="text-2xl font-bold">Resume Builder</Link>
             <button className="text-xl">{ <FaUser/> }</button>
         </nav>
