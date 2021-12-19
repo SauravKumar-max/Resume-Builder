74ad1537d61a1e56a7cde50770e80f55fe8ed1f3
Saurav Kumar <maxsaurav237@gmail.com>
2021-12-19 18:43:37 +0530

    feat: link to profile page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
 import { useRef, useEffect } from "react";
 import { useDetails } from "../context/DetailsProvider";
 
-export function Navbar(){
-    const { detailState:{ step } } = useDetails();
-    const scrollToTopRef = useRef(null);
+export function Navbar() {
+  const {
+    detailState: { step },
+  } = useDetails();
+  const navigate = useNavigate();
+  const scrollToTopRef = useRef(null);
 
-    useEffect(() => {
-        scrollToTopRef.current.scrollIntoView();
-    }, [step])
+  useEffect(() => {
+    scrollToTopRef.current.scrollIntoView();
+  }, [step]);
 
-    return(
-        <nav ref={scrollToTopRef} className="text-white bg-primary p-4 sticky flex items-center justify-between"> 
-            <Link to="/" className="text-2xl font-bold">Resume Builder</Link>
-            <button className="text-xl">{ <FaUser/> }</button>
-        </nav>
-    )
+  return (
+    <nav
+      ref={scrollToTopRef}
+      className="text-white bg-primary p-4 sticky flex items-center justify-between"
+    >
+      <Link to="/" className="text-2xl font-bold">
+        Resume Builder
+      </Link>
+      <button onClick={() => navigate("/profile")} className="text-xl">
+        {<FaUser />}
+      </button>
+    </nav>
+  );
 }
