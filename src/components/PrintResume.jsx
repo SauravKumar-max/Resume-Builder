dc807940753e5910efee8d2e32ba09749ef378e4
Saurav Kumar <maxsaurav237@gmail.com>
2022-01-25 23:55:02 +0530

    feat: added multiple template

--- src/components/PrintResume.jsx
+++ src/components/PrintResume.jsx
@@ -1,13 +1,36 @@
 import ReactToPrint from "react-to-print";
-import { ResumeTemplate } from "./ResumeTemplate";
 import { useRef } from "react";
+import { TemplateOne } from "./ResumeTemplates/TemplateOne";
+import { TemplateTwo } from "./ResumeTemplates/TemplateTwo";
+import { useDetails } from "../context/DetailsProvider";
 
 export function PrintResume() {
   const componentRef = useRef();
+  const { detailState, dispatchDetail } = useDetails();
+  const { template } = detailState;
   return (
-    <div className="w-11/12  mx-auto my-10 p-5 border-2 border-coolGray rounded shadow-xl">
-      <div className="w-full m-auto transform scale-100 md:scale-75 border border-grey overflow-hidden">
-        <ResumeTemplate refrence={componentRef} />
+    <div className="w-full  mx-auto my-2 p-2 border-2 border-coolGray rounded shadow-xl">
+      <div className="flex flex-col md:flex-row w-full m-auto scale-50 md:scale-75 transform overflow-hidden">
+        <div
+          className={`border-2 ${
+            template === "one" ? "border-red-500" : "border-grey"
+          } m-2 cursor-pointer	`}
+          onClick={() =>
+            dispatchDetail({ type: "CHOOSE_TEMPLATE", payload: "one" })
+          }
+        >
+          <TemplateOne refrence={template === "one" ? componentRef : null} />
+        </div>
+        <div
+          className={`border-2 ${
+            template === "two" ? "border-red-500" : "border-grey"
+          } m-2 cursor-pointer	`}
+          onClick={() =>
+            dispatchDetail({ type: "CHOOSE_TEMPLATE", payload: "two" })
+          }
+        >
+          <TemplateTwo refrence={template === "two" ? componentRef : null} />
+        </div>
       </div>
       <div className="flex align-center justify-center mt-3">
         <ReactToPrint
