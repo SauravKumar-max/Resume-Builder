fb8c398a31a9705eb4288066aa654fa993ed3c35
Saurav Kumar <maxsaurav237@gmail.com>
2022-04-04 23:18:20 +0530

    added Analyzer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 export { Home } from "./Home";
 export { Login } from "./Login";
 export { SignUp } from "./Signup";
-export { Details } from "./Details";
 export { Profile } from "./Profile";
+export { Details } from "./Details";
+export { Analyser } from "./Analyser";
