9b0ea849031697165084d17791a14cc84008abc5
Saurav Kumar <maxsaurav237@gmail.com>
2022-04-04 23:17:26 +0530

    added Analyze resume feature

--- src/components/PrintResume.jsx
+++ src/components/PrintResume.jsx
@@ -3,18 +3,34 @@ import { useRef } from "react";
 import { TemplateOne } from "./ResumeTemplates/TemplateOne";
 import { TemplateTwo } from "./ResumeTemplates/TemplateTwo";
 import { useDetails } from "../context/DetailsProvider";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export function PrintResume() {
+  const navigate = useNavigate();
   const componentRef = useRef();
   const { detailState, dispatchDetail } = useDetails();
-  const { template } = detailState;
+  const { template, skills } = detailState;
+  const userSkill = skills.map((i) => i.skill);
+
+  async function addSkills() {
+    navigate("/analyser");
+    try {
+      const api = "https://resume-builder.sauravkumar007.repl.co/analyser";
+      const response = await axios.post(api, { skills: userSkill });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="w-full  mx-auto my-2 p-2 border-2 border-coolGray rounded shadow-xl">
       <div className="flex flex-col md:flex-row w-full m-auto scale-50 md:scale-75 transform overflow-hidden">
         <div
-          className={`border-2 border-grey ${
+          className={`border-2 border-grey m-2 cursor-pointer ${
             template === "one" ? "opacity-100" : "opacity-50"
-          } m-2 cursor-pointer	`}
+          }`}
           onClick={() =>
             dispatchDetail({ type: "CHOOSE_TEMPLATE", payload: "one" })
           }
@@ -32,7 +48,7 @@ export function PrintResume() {
           <TemplateTwo refrence={template === "two" ? componentRef : null} />
         </div>
       </div>
-      <div className="flex align-center justify-center mt-3">
+      <div className="flex align-center justify-center">
         <ReactToPrint
           trigger={() => (
             <button className="bg-primary text-white text-lg font-bold px-4 py-2 hover:bg-btn_hover rounded">
@@ -42,6 +58,12 @@ export function PrintResume() {
           content={() => componentRef.current}
         />
       </div>
+      <button
+        className="bg-primary text-white text-lg font-bold my-4 px-4 py-2 hover:bg-btn_hover rounded"
+        onClick={addSkills}
+      >
+        Analyze Resume
+      </button>
     </div>
   );
 }
