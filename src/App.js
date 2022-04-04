df8985f9fc9cb578b4546f6a91466f5c6f7b2e0d
Saurav Kumar <maxsaurav237@gmail.com>
2022-04-04 23:16:13 +0530

    added Analyser

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom"
-import { Navbar } from './components';
-import { Home, Login, SignUp, Details, Profile } from "./pages";
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components";
+import { Home, Login, SignUp, Details, Profile, Analyser } from "./pages";
 import { PrivateRoute } from "./helpers/PrivateRoute";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="/login" element={ <Login/> } />
-        <Route path="/signup" element={ <SignUp/> } />
-        <Route 
-          path="/details" 
-          element={ 
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/details"
+          element={
             <PrivateRoute>
-              <Details/>
-            </PrivateRoute> }
+              <Details />
+            </PrivateRoute>
+          }
         />
-        <Route 
-          path="/profile" 
-          element={ 
+        <Route
+          path="/profile"
+          element={
             <PrivateRoute>
-              <Profile/>
-            </PrivateRoute> }
+              <Profile />
+            </PrivateRoute>
+          }
         />
-      </Routes> 
+        <Route
+          path="/analyser"
+          element={
+            <PrivateRoute>
+              <Analyser />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </div>
   );
 }
